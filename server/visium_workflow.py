1a3f2dbd78a8185181bbba0e7f6942c8abc086a0
moyrsd <me22b120@smail.iitm.ac.in>
2025-10-29 07:31:21 +0530

    feat : implement agentic architecture

--- server/visium_workflow.py
+++ server/visium_workflow.py
@@ -22,22 +22,6 @@ llm = ChatGoogleGenerativeAI(
 )
 
 
-class CodeGenaratorState(BaseModel):
-    code: str
-    codes: Annotated[list, operator.add]
-
-
-class CodeReviewer(BaseModel):
-    rewrite: Literal["required", "not required"] = Field(
-        "not required",
-        json_schema_extra="Check if rewrite of the code is required or not",
-    )
-    feedback: str = Field(
-        None,
-        json_schema_extra="If code rewrite is required what changes to make suggest so that the geometry is exactly like described",
-    )
-
-
 class Script(BaseModel):
     dialouge: str = Field(
         None, json_schema_extra="What will the narrator say at this part of the video"
@@ -64,7 +48,41 @@ class State(TypedDict):
     topic: str
     script: List[Script]
     directions: List[str]
-    codes: Annotated[list, operator.add]
+    code: str
+    rewrite: Literal["required", "not required"] = Field(
+        "not required",
+        json_schema_extra="Check if rewrite of the code is required or not",
+    )
+    feedback: str = Field(
+        None,
+        json_schema_extra="If code rewrite is required what changes to make suggest so that the geometry is exactly like described",
+    )
+
+
+class CodingReviewState(BaseModel):
+    rewrite: Literal["required", "not required"] = Field(
+        "not required",
+        json_schema_extra="Check if rewrite of the code is required or not",
+    )
+    feedback: str = Field(
+        None,
+        json_schema_extra="If code rewrite is required what changes to make suggest so that the geometry is exactly like described",
+    )
+
+
+class CodingWorkerState(BaseModel):
+    topic: str
+    directions: str
+    direction: str
+    rewrite: Literal["required", "not required"] = Field(
+        "not required",
+        json_schema_extra="Check if rewrite of the code is required or not",
+    )
+    feedback: str = Field(
+        None,
+        json_schema_extra="If code rewrite is required what changes to make suggest so that the geometry is exactly like described",
+    )
+    codes: List[str]
 
 
 def script_writer(state: State):
@@ -80,6 +98,7 @@ def director(state: State):
     msg = director_llm.invoke(
         f"""You are a exceptional director, you have to give exact visual instructions for a manim coding agent in natural langguage what will actually , we are making a video on topic {state["topic"]} and here is the script {state["script"]},
          # Rules 
+         - Each instructions should make sense independently,every directions will be given to separate workers unaware of other direction
          - you have to give detailed instructions on each visual action
          - Dont keep anything vague, the next person is not going to fig out 
          - The next person is dumb explain what shape, which position, what movement, what angle , from where it should originate everything
@@ -91,11 +110,54 @@ def director(state: State):
 
 
 def code_generator(state: State):
-    pass
+    if state["rewrite"] == "required":
+        feedback = state["feedback"]
+        msg = llm.invoke(
+            f"""You are an exceptional coder in manim library in python. You are making a part of the video on the topic {state["topic"]}, the directions for you are as {state["directions"]}
+            this is the previous code {state["code"]} and feedback for the code {feedback}
+            # Rules
+            - Use $ for latex wherever necessary, before every symbol use $
+            - Check if it follows the directions completly 
+            - The animations should be geomtrically correct
+            - Only return the code 
+            - Import all the things that are required 
+            """
+        )
+
+    else:
+        msg = llm.invoke(
+            f"""You are an exceptional coder in manim library in python. You are making a part of the video on the topic {state["topic"]}, the directions for you are as {state["directions"]}
+            # Rules
+            - Use $ for latex wherever necessary, before every symbol use $
+            - Only return the code 
+            - Import all the things that are required 
+            """
+        )
+
+    return {"code": msg.content}
 
 
 def code_reviewer(state: State):
-    pass
+    reviewer = llm.with_structured_output(CodingReviewState)
+    msg = reviewer.invoke(
+        f"""
+        Check if the code {state["code"]} is following all the directions {state["directions"]} and if not give feedback, and how to improve 
+
+        # Rules
+        - Use $ for latex wherever necessary, before every symbol use $
+        - Check for syntax mistakes
+        - Check if the code is following latest syntax
+        """
+    )
+    print(msg.feedback)
+    return {"rewrite": msg.rewrite, "feedback": msg.feedback}
+
+
+def route_code_review(state: State):
+    if state["rewrite"] == "required":
+        return "Rejected"
+    else:
+        return "Accepted"
 
 
 def final_json_creator(state: State):
@@ -115,9 +177,21 @@ visium_workflow.add_node("final_json_creator", final_json_creator)
 # Edges
 visium_workflow.add_edge(START, "script_writer")
 visium_workflow.add_edge("script_writer", "director")
-visium_workflow.add_edge("director", END)
+visium_workflow.add_edge("director", "code_generator")
+visium_workflow.add_edge("code_generator", "code_reviewer")
+visium_workflow.add_conditional_edges(
+    "code_reviewer", route_code_review, {"Accepted": END, "Rejected": "code_generator"}
+)
 
 
 workflow = visium_workflow.compile()
-output = workflow.invoke({"topic": "pythagoras theorem"})
-print(output["directions"])
+initial_state = {
+    "topic": "pythagoras theorem",
+    "script": [],
+    "directions": [],
+    "code": "",
+    "rewrite": "not required",
+    "feedback": "",
+}
+output = workflow.invoke(initial_state)
+print(output["code"])
